24bcce65fb9a1605e1a663603ad6443e32e3f71d
cFoschini <cjfoschini25@gmail.com>
2021-06-07 17:36:06 -0400

    updated v

---
+++ team-builder/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Members from "./Members"
+
+function App() {
+  return (
+    <div className="App">
+      <Members/>
+    </div>
+  );
+}
+
+export default App;
---
+++ team-builder/src/Members.js
@@ -0,0 +1,51 @@
+import React, {useState} from 'react'
+
+export default function Members() {
+
+    const [member, setMember] = useState([{
+        name: "Christian",
+        age: "22",
+        occupation: "Sub-contractor",
+        bio: "This is a bio!",
+        id: 1
+    },{
+        name: "John",
+        age: "34",
+        occupation: "Senior-web-dev",
+        bio: "This is a bio!",
+        id: 2
+    },{
+        name: "Tessa",
+        age: "20",
+        occupation: "Nanny",
+        bio: "This is a bio!",
+        id: 3
+    },{
+        name: "Mark",
+        age: "43",
+        occupation: "Day-trader",
+        bio: "This is a bio!",
+        id: 4
+    }]);
+
+const handleDel = (id) => { 
+    const newMembers = member.filter(mem => mem.id !== id);
+    setMember(newMembers);
+}
+
+
+
+    return (
+        <div className="member-card">
+            {member.map((mem) => (
+                <div className="team" key={mem.id}>
+                <h3>Name: {mem.name}</h3>
+                <h4>Age: {mem.age}</h4>
+                <h4>Occupation: {mem.occupation}</h4>
+                <p>Bio: {mem.bio}</p>
+                <button onClick={() =>handleDel(mem.id)}>Delete</button>
+                </div>
+            ))}
+            </div>
+    );
+}
